50a073a5b939b1bb7749cdd8ec03203523cc26ac
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-06-06 21:08:34 -0300

    FEAT: Create API for Restaurant #6

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,3 +2,4 @@ from .category import Category
 from .telephone import Telephone
 from .address import Address
 from .client import Client
+from .restaurant import Restaurant
--- core/models/restaurant.py
+++ core/models/restaurant.py
@@ -2,11 +2,13 @@ from django.db import models
 
 from usuario.models import Usuario
 
-class Client(models.Model):
-
+class Restaurant(models.Model):
     user = models.OneToOneField(Usuario, on_delete=models.PROTECT, related_name="restaurant")
-    date_birth = models.DateField()
-    cnpj = models.CharField(max_length=11, null=True, verbose_name="CNPJ")
+    name = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=14, null=True, blank=True, verbose_name="CNPJ")
+    average_delivery_time = models.SmallIntegerField()
+    description = models.CharField(max_length=255)
+    note = models.DecimalField(max_digits=2, decimal_places=1)
 
     def __str__(self):
-        return f"{self.user.email}"
+        return f"{self.user.name}"
