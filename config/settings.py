12c7894dc60082885fe505ea4bc47193061dc656
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-06-13 16:48:06 -0300

    FIX: Remove whitenoise.

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,11 @@ INSTALLED_APPS = [
     'uploader',
     'core',
     'corsheaders',
-
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <- AQUI
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -102,7 +100,6 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Corrigido para suportar collectstatic
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
