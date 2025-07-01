a10a112b913dc76058d2afae6a110215a956af4b
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-07-01 15:42:44 -0300

    FIX: Fix API of Vehicle #30

--- core/views/vehicle.py
+++ core/views/vehicle.py
@@ -4,18 +4,22 @@ from rest_framework import status
 
 from core.models import Vehicle
 from core.serializers import VehicleSerializer
-from core.views.view_helpers import verify_is_active
+from core.views.view_helpers import verify_group_user
 
 class VehicleViewSet(ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
 
     def create(self, request):
-        verify = verify_is_active(request.data)
+        verify = verify_group_user(request.data['idNaturalPerson'], 'deliverymen')
 
-        if verify == True:
-            return Response({"message": "User is already active"}, status=status.HTTP_400_BAD_REQUEST)
+        if verify == False:
+            return Response({"message": "User is not a Delivery Men"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        file = request.FILES['file']
+        request.data['file'] = file
 
+        # print(request.data['file'].file.getvalue())
         serializer =  VehicleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
