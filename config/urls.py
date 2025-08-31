b9db771a83e75c7ab4ce67d9fa306a01980fe54f
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-08-31 20:24:10 -0300

    REFACTOR: Add view in perms_map of DjangoModelPermissions #35

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core.views import *
 
@@ -41,5 +45,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/users/', include(usuario_router.urls)),
-    path('api/users/register/person/', UserRegisterAPIView.as_view())
+    path('api/users/register/person/', UserRegisterAPIView.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
