4fe3e3708c0211df52b152a19ad362fe8363806c
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-09-01 20:32:31 -0300

    REFACTOR: Change User view #35

--- core/models/natural_person.py
+++ core/models/natural_person.py
@@ -4,6 +4,7 @@ from usuario.models import Usuario
 
 class NaturalPerson(models.Model):
     user = models.OneToOneField(Usuario, on_delete=models.PROTECT, related_name="person")
+    name = models.CharField(max_length=100)
     date_birth = models.DateField()
     cpf = models.CharField(max_length=11, null=True, verbose_name="CPF")
 
