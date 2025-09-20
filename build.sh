2bd64755fd4b062a997545fa508bd8ea563b36f3
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-09-20 00:00:38 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,17 @@
 # Sai do script se houver algum erro
 set -o errexit
 
+export CARGO_HOME=$HOME/.cargo
+export RUSTUP_HOME=$HOME/.rustup
+export PATH=$CARGO_HOME/bin:$PATH
+
 # Atualiza o pip
 pip install --upgrade pip
 
-# Instala as dependências
+# Atualiza pip/setuptools/wheel para pegar wheels pré-compilados
+pip install --upgrade pip setuptools wheel
+
+# Instala dependências do seu projeto
 pip install -r requirements.txt
 
 # Coleta os arquivos estáticos
